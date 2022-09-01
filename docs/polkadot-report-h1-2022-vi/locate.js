23895bb01e3b779525c4710f65db189c858cca9a
djhatrang <103566796+hatrangkoni@users.noreply.github.com>
2022-09-01 12:04:07 +0700

    update vietnamese version

--- docs/polkadot-report-h1-2022-vi/locate.js
+++ docs/polkadot-report-h1-2022-vi/locate.js
@@ -1,22 +1,22 @@
 var lang = {
-	ksmPrice: 'KSM Price',
-	dotPrice: 'Dot Price',
-	developmentActivity: 'Development Activity',
-	totalAmount: 'Total amount',
+	ksmPrice: 'Giá KSM',
+	dotPrice: 'Giá DOT',
+	developmentActivity: 'Hoạt động Phát triển',
+	totalAmount: 'Tổng',
 	proposal: 'Proposal',
 	tips: 'Tips',
 	bounties: 'Bounties',
 	burnt: 'Burnt',
-	income: 'Income',
-	output: 'Output',
-	treasury: 'Treasury',
-	totalVCs: 'Total VCs',
-	dotVCsInvesting: 'VCs Investing in Polkadot',
-	notUsed: 'Not used',
-	usedOccasionally: 'Have used occasionally',
-	useSometimes: 'Use sometimes',
-	useFrequently: 'Use frequently',
-	others: 'Others'
+	income: 'Đầu vào',
+	output: 'Đầu ra',
+	treasury: 'Ngân quỹ',
+	totalVCs: 'Tổng số Quỹ Đầu tư',
+	dotVCsInvesting: 'Quỹ Đầu tư nắm giữ DOT',
+	notUsed: 'Không dùng',
+	usedOccasionally: 'Ít khi dùng',
+	useSometimes: 'Thỉnh thoảng dùng',
+	useFrequently: 'Thường xuyên dùng',
+	others: 'Khác'
 };
 
 window.SubWallet_Landing = window.SubWallet_Landing || {};
